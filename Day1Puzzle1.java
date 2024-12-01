8d109b1c06f349100a9947bdb8a5fe8bf2151210
Anthony Galea <anthonycgalea@gmail.com>
2024-12-01 11:28:14 -0500

    Day 1

---
+++ Day1Puzzle1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day1Puzzle1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input:");
+        String input = "";
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        while(!input.equals("q")) {
+            input = sc.nextLine();
+            if (input.length() >= 2) {
+                l1.add(Integer.parseInt(input.split("   ")[0]));
+                l2.add(Integer.parseInt(input.split("   ")[1]));
+            }
+        }
+        sc.close();
+        Collections.sort(l1);
+        Collections.sort(l2);
+        int diff = 0;
+        for (int i = 0; i < l1.size(); i++) {
+            diff += Math.abs(l1.get(i)-l2.get(i));
+        }
+        System.out.println(""+diff);
+    }
+
+}
---
+++ Day1Puzzle2.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Day1Puzzle2 {
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input:");
+        String input = "";
+        List<Integer> l1 = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        while(!input.equals("q")) {
+            input = sc.nextLine();
+            if (input.length() >= 2) {
+                l1.add(Integer.parseInt(input.split("   ")[0]));
+                int numToAdd = Integer.parseInt(input.split("   ")[1]);
+                map.put(numToAdd, 1+map.getOrDefault(numToAdd, 0));
+            }
+        }
+        sc.close();
+        int diff = 0;
+        for (int i = 0; i < l1.size(); i++) {
+            diff += Math.abs(l1.get(i)*map.getOrDefault(l1.get(i), 0));
+        }
+        System.out.println(""+diff);
+    }
+
+}
